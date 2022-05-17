866305303c8184ed08bf3c9e9ee205989b2885c1
Masaki Haga <m.haga@sonicmoov.com>
2022-05-17 18:38:34 +0900

    props down, event up

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
 	<div>
 		<h1>Hello from App.vue</h1>
-		<Child greet='Hello with props'/>
-		<!--
-			<p>parent_num: {{ parent_num }}</p>
-			<Child @my-click='parent_num = $event'/>
+		<!-- 挙動がよくわからん(上書きできない??)
+		<ChildSample greet='Hello with props'/>
 		-->
+		<p>parent_num: {{ parent_num }}</p>
+		<ChildSample @my-click='parent_num = $event'/>
 	</div>
 </template>
 
 <script>
-	import Child from './components/Child.vue'
+	import ChildSample from './components/ChildSample.vue'
 	export default {
-		// data: function(){
-		//   return{
-		//	   parent_num: 100
-		//	 }
-		// },
+		name: 'App',
 		components: {
-			Child
+			ChildSample
+		},
+		data(){
+			return{
+				parent_num: 100
+			}
 		}
 	}
 </script>
