b265ccac0ee272d060b1d6f3bf7cc751227ee391
Masaki Haga <m.haga@sonicmoov.com>
2022-05-18 11:04:57 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
 	<div>
 		<h1>Hello from App.vue</h1>
-		<!-- 挙動がよくわからん(上書きできない??)
-		<ChildSample greet='Hello with props'/>
-		-->
 		<p>parent_num: {{ parent_num }}</p>
-		<ChildSample @my-click='parent_num = $event'/>
+		<ChildSample greet='Hello with props' @my-click='parent_num = $event'/>
 	</div>
 </template>
 
