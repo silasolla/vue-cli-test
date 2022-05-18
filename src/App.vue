15127ef25c74ab0a756d613bb468fecc6d7ed6e4
Masaki Haga <m.haga@sonicmoov.com>
2022-05-18 17:41:01 +0900

    sfc test

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,51 @@
 <template>
-	<div>
-		<h1>Hello from App.vue</h1>
-		<p>parent_num: {{ parent_num }}</p>
-		<ChildSample greet='Hello with props' @my-click='parent_num = $event'/>
+	<div class="red-b flex-container">
+		<div v-for="card of cards" :key="card">
+			<CardSample :id="card.id" :title="card.title" :image="card.image" :description="card.description"/>
+		</div>
 	</div>
 </template>
 
 <script>
-	import ChildSample from './components/ChildSample.vue'
+	import CardSample from '@/components/CardSample'
 	export default {
 		name: 'App',
 		components: {
-			ChildSample
+			CardSample
 		},
 		data(){
 			return{
-				parent_num: 100
+				cards: [
+					{
+						id: 1,
+						title: 'タイトル1',
+						image: 'img1.jpg',
+						description: '詳細1'	
+					},
+					{
+						id: 2,
+						title: 'タイトル2',
+						image: 'img2.jpg',
+						description: '詳細2'	
+					},
+					{
+						id: 3,
+						title: 'タイトル3',
+						image: 'img3.jpg',
+						description: '詳細3'	
+					}
+				]
 			}
 		}
 	}
 </script>
+
+<style scoped>
+	.red-b{
+		border: 1px solid red;
+	}
+	.flex-container {
+		display: flex;
+		justify-content: space-around; 
+	}
+</style>
---
+++ src/components/CardSample.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<div class="blue-b mb-4">
+			<p>id: {{ id }}</p>
+			<p>title: {{ title }}</p>
+			<p>image: {{ image }}</p>
+			<p>description: {{ description }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			id: Number,
+			title: String,
+			image: String,
+			description: String
+		}
+	}
+</script>
+
+<style scoped>
+	.blue-b{
+		border: 1px solid blue;
+	}
+	.mb-4{
+		margin: 4px;
+		padding: 4px;
+	}
+</style>
